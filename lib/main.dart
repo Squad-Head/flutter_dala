1cd0d35c65465bbed7c612f2305b0ac12238b04e
ambanikousik <rikousik@gmail.com>
2022-03-14 09:39:29 +0600

    shifted to flutter design

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_ui_toolkit/presentation/dala_app.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_design/flutter_design.dart';
+import 'package:flutter_design_viewer/flutter_design_viewer.dart';
+import 'package:flutter_ui_toolkit/docs.dart';
+import 'package:flutter_ui_toolkit/page_factory.design.dart';
+
+import 'data_builders.dart';
 
 Future<void> main() async {
-  runApp(const ProviderScope(child: DalaApp()));
+  final themes = {
+    'light': ThemeData.light(),
+    'dark': ThemeData.dark(),
+  };
+  runApp(
+    DesignSystemViewerApp(
+      settings: ViewerSettings(
+        enabledLocales: {
+          'en-US': const Locale('en', 'US'),
+        },
+        enabledThemes: themes,
+        widgetDisplayHeight: 500,
+      ),
+      dataBuilders: {
+        String: [
+          // Need to use a condition here to use the data builder constructor's default value
+          ([p]) => p != null && p is String
+              ? MyStringDataBuilder(p)
+              : MyStringDataBuilder(),
+        ]
+      },
+      branding: Text('Yoo'),
+      pageGroups: [
+        // Your custom pages
+        ...buildGroupedPageTrees(docPages),
+        // Theme page
+        // buildThemePageGroup(themes: themes),
+        // Generated @design/@Design annotated pages
+        buildComponentPageTree(componentPages: generatedComponentPages),
+      ],
+    ),
+  );
 }
